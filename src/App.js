459c2b10e059eb0cb2a9616b7e0465e6b4b5ef23
Prashanth <yvprashanth@gmail.com>
2018-01-31 16:58:02 -0800

    can save form

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ class App extends Component {
     })
   }
 
+  createContact(contact){
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }));
+    });
+  }
+
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
@@ -31,7 +39,13 @@ class App extends Component {
             contacts={this.state.contacts}
           />
         )}/>
-        <Route path='/create' component={CreateContact}/>
+        <Route path='/create' component={({history}) => (
+          <CreateContact onCreateContact={(contact) => {
+            this.createContact(contact)
+            history.push('/')
+          }} 
+          />
+        )}/>
       </div>
     )
   }
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
-  render () {
-    var divStyle = {
-      'text-align': 'center',
-      'font-weight': 'bolder'
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const values = serializeForm(e.target, {hash:true});
+    if(this.props.onCreateContact){
+      this.props.onCreateContact(values);
     }
+  }
+
+
+  render () {
     return (
-      <div style={divStyle}>
+      <div>
         <Link className='close-create-contact' to='/'> Close
         </Link>
-        <form className='create-contact-form'>
+        <form onSubmit={this.handleSubmit} className='create-contact-form'>
           <ImageInput className='create-contact-avatar-input' name='avatarURL' maxHeight={64} />
           <div className='create-contact-details'>
             <input type='text' name='name' placeholder='Name' />
